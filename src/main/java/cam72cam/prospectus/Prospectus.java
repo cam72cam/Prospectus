45fb40c67cf9bcf7670e7ff207b5657c9eaac710
Christian Mesh <christianmesh1@gmail.com>
2018-02-02 16:44:24 -0500

    Initial commit

---
+++ src/main/java/cam72cam/prospectus/Prospectus.java
@@ -0,0 +1,17 @@
+package cam72cam.prospectus;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = Prospectus.MODID, version = Prospectus.VERSION)
+public class Prospectus
+{
+    public static final String MODID = "prospectus";
+    public static final String VERSION = "0.1";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+    }
+}
