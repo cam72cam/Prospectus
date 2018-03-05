3e2dbb97af03b2ac51ecfe180efddd7b488bba27
Christian Mesh <christianmesh1@gmail.com>
2018-03-05 08:02:55 -0500

    Add items

---
+++ src/main/java/cam72cam/prospectus/ProspectorPickItem.java
@@ -0,0 +1,90 @@
+package cam72cam.prospectus;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.world.World;
+
+public class ProspectorPickItem extends Item {
+	public static final String NAME = "item_prospector_";
+	
+	private final int radius;
+	public final int accuracy;
+	
+	public ProspectorPickItem(String type, int radius, int accuracy) {
+		super();
+		setUnlocalizedName(Prospectus.MODID + ":" + NAME + type);
+        setRegistryName(new ResourceLocation(Prospectus.MODID, NAME + type));
+        this.setCreativeTab(CreativeTabs.TOOLS);
+        this.setMaxStackSize(1);
+        this.setMaxDamage(100);
+        
+		this.radius = radius; 
+		this.accuracy = accuracy;
+	}
+	
+	@Override
+	public EnumActionResult onItemUse(EntityPlayer player, World world, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+		if (world.isRemote) {
+			return EnumActionResult.PASS;
+		}
+		
+		player.getHeldItem(hand).damageItem(1, player);
+		
+		Map<String, Integer> counts = new HashMap<String, Integer>();
+		
+		for (int x = -radius; x < radius; x++) {
+			for (int y = 0; y < radius; y++) {
+				for (int z = -radius; z < radius; z++) {
+					if (Math.random() * 100 < accuracy) {
+						BlockPos curr = pos.down(y).east(x).north(z);
+						IBlockState state = world.getBlockState(curr);
+						//String name = state.getBlock().getLocalizedName();
+						@SuppressWarnings("deprecation")
+						String name = state.getBlock().getItem(world, curr, state).getDisplayName();
+						if (! name.toLowerCase().contains("ore")) {
+							continue;
+						}
+						if (!counts.containsKey(name) ) {
+							counts.put(name, 0);
+						}
+						counts.put(name, counts.get(name) + 1);
+					}
+				}
+			}
+		}
+		
+		player.sendMessage(new TextComponentString("Found: "));
+		for (String key : counts.keySet()) {
+			String val = "";
+			Integer count = counts.get(key);
+			double scale =  Math.pow(radius / 16, 2) * (accuracy / 5);
+			if (count < 5 * scale) {
+				val = "A Few ";
+			} else if (count < 10 * scale) {
+				val = "A Handfull of ";
+			} else if (count < 20 * scale) {
+				val = "A Bunch of ";
+			} else if (count < 30 * scale) {
+				val = "A Ton of ";
+			} else {
+				val = "The Motherload of ";
+			}
+			
+			player.sendMessage(new TextComponentString("  " + val + key));
+		}
+		
+		
+		return EnumActionResult.PASS;
+	}
+}
--- src/main/java/cam72cam/prospectus/Prospectus.java
+++ src/main/java/cam72cam/prospectus/Prospectus.java
@@ -1,17 +1,49 @@
 package cam72cam.prospectus;
 
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Mod(modid = Prospectus.MODID, version = Prospectus.VERSION)
 public class Prospectus
 {
     public static final String MODID = "prospectus";
     public static final String VERSION = "0.1";
+    public static final Item ITEM_PROSPECTOR_STONE = new ProspectorPickItem("stone", 16, 5);
+    public static final Item ITEM_PROSPECTOR_IRON = new ProspectorPickItem("iron", 16, 20);
+    public static final Item ITEM_PROSPECTOR_DIAMOND = new ProspectorPickItem("diamond", 16, 60);
+    
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+    	MinecraftForge.EVENT_BUS.register(this);
+    }
     
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
+    	
+    }
+    
+    @SubscribeEvent
+    public void registerItems(RegistryEvent.Register<Item> event)
+    {
+    	event.getRegistry().register(ITEM_PROSPECTOR_STONE);
+    	event.getRegistry().register(ITEM_PROSPECTOR_IRON);
+    	event.getRegistry().register(ITEM_PROSPECTOR_DIAMOND);
+    }
+    
+    @SubscribeEvent
+    public void registerModels(ModelRegistryEvent event) {
+    	ModelLoader.setCustomModelResourceLocation(ITEM_PROSPECTOR_STONE, 0, new ModelResourceLocation(ITEM_PROSPECTOR_STONE.getRegistryName(), "inventory"));
+    	ModelLoader.setCustomModelResourceLocation(ITEM_PROSPECTOR_IRON, 0, new ModelResourceLocation(ITEM_PROSPECTOR_IRON.getRegistryName(), "inventory"));
+    	ModelLoader.setCustomModelResourceLocation(ITEM_PROSPECTOR_DIAMOND, 0, new ModelResourceLocation(ITEM_PROSPECTOR_DIAMOND.getRegistryName(), "inventory"));
     }
 }
