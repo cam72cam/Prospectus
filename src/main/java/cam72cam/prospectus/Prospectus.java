b81aab56f59b1c558c2899c46a1f83a9fa00576f
Christian Mesh <christianmesh1@gmail.com>
2018-04-14 07:49:18 -0400

    Change version to v1.1

--- src/main/java/cam72cam/prospectus/Prospectus.java
+++ src/main/java/cam72cam/prospectus/Prospectus.java
@@ -20,7 +20,7 @@ import org.apache.logging.log4j.Logger;
 public class Prospectus
 {
     public static final String MODID = "prospectus";
-    public static final String VERSION = "0.2";
+    public static final String VERSION = "1.1";
     public static Item ITEM_PROSPECTOR_STONE;
     public static Item ITEM_PROSPECTOR_IRON;
     public static Item ITEM_PROSPECTOR_DIAMOND;
